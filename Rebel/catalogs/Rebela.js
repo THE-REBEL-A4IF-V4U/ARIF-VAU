0ac52d5dcc2a827909307b64e54bc37683910d82
THE REBEL ARIF VAU <123198628+THE-REBEL-A4IF-V4U@users.noreply.github.com>
2025-04-02 17:45:57 +0600

    Update Rebela.js

--- Rebel/catalogs/Rebela.js
+++ Rebel/catalogs/Rebela.js
@@ -3,11 +3,11 @@ const { spawn } = require("child_process");
 const express = require("express");
 const app = express();
 const chalk = require('chalk');
-const logger = require("./rebelc.js");
+const logger = require("./Rebelc.js");
 const path = require('path');
 const PORT = process.env.PORT || 8080 || 9000 || 5555 || 5050 || 5000 || 3003 || 2000 || 1029 || 1010;
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/website/rebel.html'));
+  res.sendFile(path.join(__dirname, '/website/Rebel.html'));
 });
 console.clear();
 function startBot(message) {
@@ -15,7 +15,7 @@ function startBot(message) {
   console.log(chalk.blue('MODIFIED BY REBEL BOOSTING\n'));
   logger.loader(`deploying app on port ${chalk.blueBright(PORT)}`);
   app.listen(logger.loader(`app deployed on port ${chalk.blueBright(PORT)}`));
-  const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "rebelb.js"], {
+  const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "Rebelb.js"], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true
