b6bbb82a095690dc64b7205c4cdc9d0f5248191d
THE REBEL ARIF VAU <123198628+THE-REBEL-A4IF-V4U@users.noreply.github.com>
2025-04-03 04:03:54 +0600

    Update inbox.js

--- scripts/commands/inbox.js
+++ scripts/commands/inbox.js
@@ -1,22 +1,10 @@
-module.exports.config = { name: "inbox", version: "1.0", permission: 0, credits: "Rebel", description: "Send an image with a message to a user's inbox", category: "general", usages: "inbox", prefix: true, cooldowns: 5, dependencies: "", };
+module.exports.config = { name: "inbox", version: "1.0", permission: 2, credits: "Rebel", description: "Send a message to a user's inbox", category: "spam", usages: "inbox", prefix: true, cooldowns: 5, dependencies: "", };
 
 module.exports.run = async function ({ api, event, Users, args }) { var userID = Object.keys(event.mentions)[0] || event.senderID; var userName = await Users.getNameUser(userID);
 
-const messageText = ✅ SUCCESSFULLY SENT MESSAGE\n\n🔰 [${userName}] PLEASE CHECK YOUR INBOX OR MESSAGE REQUEST BOX.;
+if (!args[0]) { api.sendMessage(✅ SUCCESSFULLY SENT MESSAGE\n\n🔰 [${userName}] PLEASE CHECK YOUR INBOX OR MESSAGE REQUEST BOX, event.threadID); }
 
- const imageUrl = "https://drive.google.com/uc?export=download&id=1EfUxyNQzXhItnzvL3qRQWOyaP765nd2m"; 
+const confirmationMessage = ✅ SUCCESSFULLY ALLOW\n🔰 NOW YOU CAN USE ${global.config.BOTNAME} HERE;
 
-try { const attachment = await global.utils.getStreamFromURL(imageUrl); var messageData = { body: "✅ SUCCESSFULLY ALLOW\n🔰 NOW YOU CAN USE " + global.config.BOTNAME + " HERE", attachment: attachment };
-
-// Send confirmation message to the group
-api.sendMessage(messageText, event.threadID);
-
-// Send the message with an image to the user's inbox
-api.sendMessage(messageData, userID, (err) => {
-  if (err) {
-    api.sendMessage("❌ Failed to send image to inbox.", event.threadID);
-  }
-});
-
-} catch (error) { api.sendMessage("❌ Error fetching the image.", event.threadID); } };
+try { api.sendMessage(confirmationMessage, userID); } catch (error) { api.sendMessage("❌ Failed to send message to inbox.", event.threadID); } };
 
