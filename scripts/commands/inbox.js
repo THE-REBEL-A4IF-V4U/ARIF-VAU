04ab776856c4772282f74db619d5a0ff46281fff
THE REBEL ARIF VAU <123198628+THE-REBEL-A4IF-V4U@users.noreply.github.com>
2025-04-03 03:59:41 +0600

    Update inbox.js

--- scripts/commands/inbox.js
+++ scripts/commands/inbox.js
@@ -1,11 +1,10 @@
-module.exports.config = { name: "inbox", version: "1.0", permission: 0, // Changed to 0 so everyone can use it 
-                         credits: "Rebel", description: "Send an image with a message to a user's inbox", category: "general", usages: "inbox", prefix: true, cooldowns: 5, dependencies: "", };
+module.exports.config = { name: "inbox", version: "1.0", permission: 0, credits: "Rebel", description: "Send an image with a message to a user's inbox", category: "general", usages: "inbox", prefix: true, cooldowns: 5, dependencies: "", };
 
 module.exports.run = async function ({ api, event, Users, args }) { var userID = Object.keys(event.mentions)[0] || event.senderID; var userName = await Users.getNameUser(userID);
 
 const messageText = ✅ SUCCESSFULLY SENT MESSAGE\n\n🔰 [${userName}] PLEASE CHECK YOUR INBOX OR MESSAGE REQUEST BOX.;
 
-// URL of the image file const imageUrl = "https://drive.google.com/uc?export=download&id=1EfUxyNQzXhItnzvL3qRQWOyaP765nd2m"; // Change this to the correct image URL
+ const imageUrl = "https://drive.google.com/uc?export=download&id=1EfUxyNQzXhItnzvL3qRQWOyaP765nd2m"; 
 
 try { const attachment = await global.utils.getStreamFromURL(imageUrl); var messageData = { body: "✅ SUCCESSFULLY ALLOW\n🔰 NOW YOU CAN USE " + global.config.BOTNAME + " HERE", attachment: attachment };
 
