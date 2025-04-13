8a4860d6b27c74b461068d830553f12e91cae9e3
THE REBEL ARIF VAU <123198628+THE-REBEL-A4IF-V4U@users.noreply.github.com>
2025-04-13 12:47:01 +0600

    Update index.js

--- Rebel/system/database/index.js
+++ Rebel/system/database/index.js
@@ -2,7 +2,7 @@
 
 const Sequelize = require("sequelize");
 const { resolve } = require("path");
-const { DATABASE } = global.Rebel;
+const { DATABASE } = global.rebel;
 
 var dialect = Object.keys(DATABASE), storage;
 dialect = dialect[0]; 
