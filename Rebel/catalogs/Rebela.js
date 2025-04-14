2b7ef41b3ab48ba925da424d65fb45f9eb4b0c9a
THE REBEL ARIF VAU <123198628+THE-REBEL-A4IF-V4U@users.noreply.github.com>
2025-04-15 01:06:15 +0600

    Update Rebela.js

--- Rebel/catalogs/Rebela.js
+++ Rebel/catalogs/Rebela.js
@@ -1,37 +1,12 @@
 const { spawn } = require("child_process");
 const chalk = require("chalk");
 const path = require("path");
-const express = require("express");
-const fs = require("fs");
-const app = express();
 const logger = require("./Rebelc.js");
 
-const PORT = process.env.PORT || 8080;
 let botProcess = null;
-let serverStarted = false;
-
-app.get("/", (req, res) => {
-  const filePath = path.join(__dirname, "./website/Rebel.html");
-  if (fs.existsSync(filePath)) {
-    res.sendFile(filePath);
-  } else {
-    res.send("<h1>Website is coming soon. Rebel.html not found.</h1>");
-  }
-});
 
 function startBot() {
   logger("Starting REBEL Bot...", "rebel");
-
-  if (!serverStarted) {
-    app.listen(PORT, () => {
-      logger.loader(`Website served on port ${chalk.blueBright(PORT)}`);
-    });
-    serverStarted = true; // Mark as started
-    logger.loader(`App running on port ${chalk.blueBright(PORT)}`);
-  } else {
-    logger("Server already running, skipping app.listen()", "warn");
-  }
-
   runBot();
 }
 
@@ -47,7 +22,7 @@ function runBot() {
     if (code !== 0) {
       logger("Restarting bot in 5 seconds...", "rebel");
       setTimeout(() => {
-        runBot();
+        runBot(); // restart bot
       }, 5000);
     }
   });
