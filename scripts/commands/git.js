a745137109cdc653022713267881c1a74178583c
THE REBEL ARIF VAU <123198628+THE-REBEL-A4IF-V4U@users.noreply.github.com>
2025-04-18 16:23:19 +0600

    Update git.js

--- scripts/commands/git.js
+++ scripts/commands/git.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	version: "1.0.0",
 	permssion: 0,
 	credits: "Rebel",
-        prefix: 'awto',
+        prefix: 'true',
 	description: "Get username's github info",
 	category: "user",
 	depndencies: {"fetch": "","node-fetch": "", "moment": ""},
